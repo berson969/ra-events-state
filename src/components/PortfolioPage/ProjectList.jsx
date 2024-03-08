a98d13aa245d94daac29bf0cdc9f50984158bd13
berson969 <berson969@gmail.com>
2024-03-08 19:49:55 +0100

    change masonry layout

--- src/components/PortfolioPage/ProjectList.jsx
+++ src/components/PortfolioPage/ProjectList.jsx
@@ -1,11 +1,8 @@
 export const ProjectList = ({ projects }) => {
     return (
-        <ul className="grid lg:grid-cols-3 grid-cols-1 md:grid-cols-2 gap-4">
-        {/*<ul className="flex flex-wrap">*/}
-
+        <ul className="columns-1 md:columns-2 lg:columns-3 gap-4">
             {projects.map((item, index) => (
                 <li key={index} className="relative  p-2 w-full">
-                {/*<li key={index} className="relative flex-none p-2 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">*/}
                     <img src={item.img} alt={item.category} className=" w-full h-auto object-cover" />
                 </li>
             ))}
